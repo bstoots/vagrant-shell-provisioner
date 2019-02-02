1fabad3d31dd533dfc4743f9f294a8604815ccfe
Brian Stoots <5334637+bstoots@users.noreply.github.com>
2019-02-02 17:43:04 -0500

    Remove work-around for old ansible localhost provisioning via temp inv file.  Reference our new hot inventory file instead.

--- config-management/ansible/ansible-playbook.sh
+++ config-management/ansible/ansible-playbook.sh
@@ -15,20 +15,6 @@ else
   echo 'Argument $2 must be one or more Ansible playbooks'
   exit 1
 fi
-# temporary inventory will be written to ~/.localhost.ini only if it doesn't already exist to avoid
-# accidental overwrites
-if [ ! -f ~/.localhost.ini ]; then
-  ansible_inventory_tmp=~/.localhost.ini
-  # le sigh ... https://github.com/ansible/ansible/issues/11695
-  echo "localhost              ansible_connection=local" > $ansible_inventory_tmp
-  if [ ! -f "$ansible_inventory_tmp" ]; then
-    echo "Temporary inventory was not written to: $ansible_inventory_tmp"
-    exit 1
-  fi
-else
-  echo "Inventory file already exists at: ~/.localhost.ini"
-  exit 1
-fi
 
 # Explode more than one playbook out into many on whitespace and prepend ansible_working_dir
 playbooks=( $ansible_playbooks )
@@ -56,8 +42,5 @@ export PYTHONUNBUFFERED=1
 # show ANSI-colored output
 export ANSIBLE_FORCE_COLOR=true
 echo "Running Ansible as $USER:"
-# Have to use the inventory tmpfile here because we can't remove executable bit on Windows
-echo "ansible-playbook $ansible_playbooks --inventory-file=$ansible_inventory_tmp --connection=local ${ansible_extra_vars_file} ${ansible_options}"
-ansible-playbook ${ansible_playbooks} --inventory-file="$ansible_inventory_tmp" --connection=local ${ansible_extra_vars_file} ${ansible_options} || true
-# Trash the temp inventory afterwards
-rm $ansible_inventory_tmp
+echo "ansible-playbook $ansible_playbooks --inventory-file /vagrant/ansible/localhost ${ansible_extra_vars_file} ${ansible_options}"
+ansible-playbook ${ansible_playbooks} --inventory-file /vagrant/ansible/localhost ${ansible_extra_vars_file} ${ansible_options} || true
